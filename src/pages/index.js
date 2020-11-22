04182fa354da92364eae97e67c9ae1cbdf65dd59
Bonnie Why <Bonnie.Why@vmlyr.com>
2020-11-21 19:29:52 -0600

    added video and cta to bottom of first block of content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import React from 'react'
 import Helmet from 'react-helmet'
 import pic01 from '../assets/images/pic01.jpg'
@@ -11,107 +11,133 @@ import Banner from '../components/Banner'
 import Layout from '../components/layout'
 
 class HomeIndex extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <Helmet
-          title="VMLY&amp;R Presents: Hour of Code"
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        ></Helmet>
+    render() {
+        return (
+            <Layout>
+                <Helmet
+                    title="VMLY&amp;R Presents: Hour of Code"
+                    meta={[
+                        {name: 'description', content: 'Sample'},
+                        {name: 'keywords', content: 'sample, something'},
+                    ]}
+                ></Helmet>
 
-        <Banner />
+                <Banner/>
 
-        <div id="main">
-          <section id="one">
-              <div className="inner">
-               <header className="major">
-                 <h2>Welcome Future Coders!</h2>
-                </header>
-                <p>
-                  This year, during Computer Science Education Week (December 7-13), we will be hosting a virtual <a href="https://hourofcode.com/us">Hour of Code</a>™  
-                  event for children in grades 6-12th grade. 
-                </p>
-                <p>
-                  Our goal is to inspire curious minds to take the first steps in 
-                  discovering how computer science impacts the world around us. We want to encourage and inspire diversity in 
-                  technology by welcoming diverse people and experiences, empowering attendees, and promoting accessibility to learning resources that will aid in continued learning and discovery.
-                </p>
-              </div>
-            </section>
-          <section id="two" className="tiles">
-            <article style={{ backgroundImage: `url(${pic01})` }}>
-              <header className="major">
-                <h3>Aliquam</h3>
-                <p>Ipsum dolor sit amet</p>
-              </header>
-            </article>
-            <article style={{ backgroundImage: `url(${pic02})` }}>
-                <header className="major">
-                  <h3>Two sessions available</h3>
-                    <p>
-                      Friday, December 11th <b>6:30pm to 8:30pm</b>
-                    </p>
-                    <p>
-                      Saturday, December 12th <b>10am to 12pm</b>
-                    </p>
-                </header>
-            </article>
-            <article style={{ backgroundImage: `url(${pic03})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Magna</h3>
-                  <p>Lorem etiam nullam</p>
-                </header>
-              </Link>
-            </article>
-            <article style={{ backgroundImage: `url(${pic04})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Ipsum</h3>
-                  <p>Nisl sed aliquam</p>
-                </header>
-              </Link>
-            </article>
-            <article style={{ backgroundImage: `url(${pic05})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Consequat</h3>
-                  <p>Ipsum dolor sit amet</p>
-                </header>
-              </Link>
-            </article>
-            <article style={{ backgroundImage: `url(${pic06})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Etiam</h3>
-                  <p>Feugiat amet tempus</p>
-                </header>
-              </Link>
-            </article>
-          </section>
-        </div>
-      </Layout>
-    )
-  }
+                <div id="main">
+                    <section id="one">
+                        <div className="inner">
+                            <header className="major">
+                                <h2>Welcome Coders!</h2>
+                            </header>
+                            <p>
+                                This year, during Computer Science Education Week (December 7-13), we will be hosting a
+                                virtual<a href="https://hourofcode.com/us">Hour of Code</a>™
+                                event for children in grades 6-12th grade.
+                            </p>
+                            <p>
+                                Our goal is to inspire curious minds to take the first steps in
+                                discovering how computer science impacts the world around us. We want to encourage and
+                                inspire diversity in
+                                technology by welcoming diverse people and experiences, empowering attendees, and
+                                promoting accessibility to learning resources that will aid in continued learning and
+                                discovery.
+                            </p>
+                            <div className="content">
+                                <div className="video">
+                                    <iframe
+                                        width="560"
+                                        height="315"
+                                        src="https://www.youtube.com/embed/SrnvvWDm73k"
+                                        frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                        allowfullscreen>
+                                    </iframe>
+                                </div>
+                                <div className="cta">
+                                    <ul className="actions">
+                                        <li>
+                                            <a href="#" name="Apply now" className="button next scrolly">
+                                                Apply now!
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section id="two" className="tiles">
+                        <article style={{backgroundImage: `url(${pic01})`}}>
+                            <header className="major">
+                                <h3>Aliquam</h3>
+                                <p>Ipsum dolor sit amet</p>
+                            </header>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic02})`}}>
+                            <header className="major">
+                                <h3>Two sessions available</h3>
+                                <p>
+                                    Friday, December 11th:
+                                    <b>6:30pm to 8:30pm</b>
+                                </p>
+                                <p>
+                                    Saturday, December 12th:
+                                    <b>10am to 12pm</b>
+                                </p>
+                            </header>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic03})`}}>
+                            <Link
+                                aria-label="Link to Landing Page"
+                                to="/landing"
+                                className="link primary"
+                            >
+                                <header className="major">
+                                    <h3>Magna</h3>
+                                    <p>Lorem etiam nullam</p>
+                                </header>
+                            </Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic04})`}}>
+                            <Link
+                                aria-label="Link to Landing Page"
+                                to="/landing"
+                                className="link primary"
+                            >
+                                <header className="major">
+                                    <h3>Ipsum</h3>
+                                    <p>Nisl sed aliquam</p>
+                                </header>
+                            </Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic05})`}}>
+                            <header className="major">
+                                <h3>Questions?</h3>
+                                <p>Contact Liz Wait -</p>
+                                <p>
+                                    <a href="mailto:elizabeth.wait@vmlyr.com?Subject=VMLYR%Hour%of%Code%Inquiry"
+                                       target="_blank">elizabeth.wait@vmlyr.com
+                                    </a>
+                                </p>
+                            </header>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic06})`}}>
+                            <Link
+                                aria-label="Link to Landing Page"
+                                to="/landing"
+                                className="link primary"
+                            >
+                                <header className="major">
+                                    <h3>Etiam</h3>
+                                    <p>Feugiat amet tempus</p>
+                                </header>
+                            </Link>
+                        </article>
+                    </section>
+                </div>
+            </Layout>
+        )
+    }
 }
 
 export default HomeIndex
